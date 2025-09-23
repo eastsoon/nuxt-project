a2747d6c4c5bca28478886be2c1aeac78ff8ecbb
jds <user@jds-mac.local>
2025-09-23 13:25:09 +0900

    15. Course 데이터 가져오기 완성

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,14 @@
         <p class="q-mt-sm text-grey-8">웹 개발 입문부터 실전까지 학습해보세요.</p>
       </div>
       <div class="row q-col-gutter-lg">
-        <div v-for="n in 3" :key="n" class="col-12 col-md-4 col-sm-6">
-          <CourseCard />
+        <div v-for="{courseSlug, title, subtitle, thumbnail, path } in courses" :key="courseSlug" class="col-12 col-md-4 col-sm-6">
+          <NuxtLink v-slot="{navigate}" custom :to="path">
+            <CourseCard 
+              :thumbnail="thumbnail"
+              :title="title"
+              :subtitle="subtitle"
+              @click="navigate"/>
+          </NuxtLink>
         </div>
       </div>
     </q-page>
