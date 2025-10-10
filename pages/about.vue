a384c1d017e01df36b557f196f61034825dcfdde
순진 <jds920805@gmail.com>
2025-10-10 23:51:37 +0900

    41. useState()

--- pages/about.vue
+++ pages/about.vue
@@ -6,7 +6,17 @@
         description="Vue & Nuxt Mastery Class 웹은 Nuxt3로 만들어졌습니다."
       />
     </div>
+    <div class="q-gutter-y-sm q-mt-md">
+      <div class="text-subtitle text-weight-bold">useState('count')</div>
+      <div>
+        count: {{ count }}
+        <q-btn label="증가" color="primary" @click="count++" />
+        <q-btn label="감소" color="primary" @click="count--" />
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const count = useState<number>("count", () => 1);
+</script>
