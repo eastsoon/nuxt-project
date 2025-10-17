7f6d491c1a11a4878e5ffc994da3d29aa9477a5f
jds <user@jds-mac.local>
2025-10-17 18:00:08 +0900

    48. Server Routes

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,20 +1,15 @@
-import type { CourseWithPath } from '~/types/course';
+import type { CourseReturn, CourseWithPath } from "~/types/course";
 
-interface CourseReturn {
-    course: Maybe<CourseWithPath>;
-    prevCourse: Maybe<CourseWithPath>;
-    nextCourse: Maybe<CourseWithPath>;
-}
 export const useCourse = (courseSlug: string): CourseReturn => {
-    const { courses } = useCourses();
-    // const course = courses.find((course) => course.courseSlug === courseSlug);
-    const index = courses.findIndex((course) => course.courseSlug === courseSlug);
-    const course = courses[index];
-    const prevCourse = courses[index - 1];
-    const nextCourse = courses[index + 1];
-    return {
-        course,
-        prevCourse,
-        nextCourse,
-    }
-}
+  const { courses } = useCourses();
+  // const course = courses.find((course) => course.courseSlug === courseSlug);
+  const index = courses.findIndex((course) => course.courseSlug === courseSlug);
+  const course = courses[index];
+  const prevCourse = courses[index - 1];
+  const nextCourse = courses[index + 1];
+  return {
+    course,
+    prevCourse,
+    nextCourse,
+  };
+};
--- types/course.ts
+++ types/course.ts
@@ -1,23 +1,28 @@
 export interface Course {
-    title: string;
-    subtitle: string;
-    courseSlug: string;
-    content: string;
-    thumbnail: string;
-    video: string;
-    rating: number;
-    reviewsCount: number;
-    studentCount: number;
-    reviewsUrl: string;
-    inflearnUrl: string;
-    gymcodingUrl: string;
-  }
+  title: string;
+  subtitle: string;
+  courseSlug: string;
+  content: string;
+  thumbnail: string;
+  video: string;
+  rating: number;
+  reviewsCount: number;
+  studentCount: number;
+  reviewsUrl: string;
+  inflearnUrl: string;
+  gymcodingUrl: string;
+}
 
 export interface CourseWithPath
-  extends Omit<Course, 'rating' | 'reviewsCount' | 'studentCount'> {
+  extends Omit<Course, "rating" | "reviewsCount" | "studentCount"> {
   path: string;
   rating: string;
   reviewsCount: string;
   studentCount: string;
 }
-  
+
+export interface CourseReturn {
+  course: Maybe<CourseWithPath>;
+  prevCourse: Maybe<CourseWithPath>;
+  nextCourse: Maybe<CourseWithPath>;
+}
